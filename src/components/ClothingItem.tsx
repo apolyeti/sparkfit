58eca7acf2db359686369f97e05128b0a4fecc49
apolyeti <azhandarveen@gmail.com>
2024-07-14 16:41:56 -0700

    feat: more styling

--- src/components/ClothingItem.tsx
+++ src/components/ClothingItem.tsx
@@ -1,28 +1,17 @@
 import type { SparkFitImage } from "@utils/types";
 import Image from "next/image";
 import { useState } from "react";
+import Modal from "@components/Clothing.Modal";
 
 interface ClothingItemProps {
-    key: string; // for mapping
     image: SparkFitImage;
 }
-
-const Modal = ({ image, onClose }: { image: SparkFitImage; onClose: () => void }) => {
-    return (
-        <div className="modal-overlay">
-            <div className="modal-content">
-                <button onClick={onClose}>Close</button>
-                <Image src={image.data} alt={`${image.name} in modal`} width={500} height={500} />
-            </div>
-        </div>
-    );
-};
 
 export default function ClothingItem({ image }: ClothingItemProps) {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center no-line">
             <div
                 className="card"
                 onClick={() => setIsModalOpen(true)}
