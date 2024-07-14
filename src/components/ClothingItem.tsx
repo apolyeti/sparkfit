86994316885447707c7c843681b8645ff1e0b3a7
apolyeti <azhandarveen@gmail.com>
2024-07-14 16:56:02 -0700

    feat: im so done with this styling

--- src/components/ClothingItem.tsx
+++ src/components/ClothingItem.tsx
@@ -1,28 +1,22 @@
 import type { SparkFitImage } from "@utils/types";
 import Image from "next/image";
-import { useState } from "react";
-import Modal from "@components/Clothing.Modal";
 
 interface ClothingItemProps {
     image: SparkFitImage;
 }
 
 export default function ClothingItem({ image }: ClothingItemProps) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
         <div className="flex flex-col items-center justify-center no-line">
             <div
                 className="card"
-                onClick={() => setIsModalOpen(true)}
             >
                 <div className="image-container">
                     <Image src={image.data} alt={`${image.name} submitted by user`} width={200} height={200} />
                 </div>
                 <p>{image.name}</p>
             </div>
-
-            {isModalOpen && <Modal image={image} onClose={() => setIsModalOpen(false)} />}
         </div>
     );
 }
