5a26f786fad33d317232cafb347ef60d05192b08
apolyeti <azhandarveen@gmail.com>
2024-07-16 16:13:34 -0700

    feat: next.config.mjs rewrites property ruined quite literally everything

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,34 @@
 import NextAuth, { NextAuthConfig} from "next-auth";
-import github from "next-auth/providers/github";
-// import { OAuthUserConfig } from "next-auth/providers";
-// import { GitHubProfile } from "next-auth/providers/github";
+import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import type { Provider } from "next-auth/providers";
 
+const providers: Provider[] = [
+    GitHub,
+    Credentials({
+        credentials: { password: { label: "Password", type: "password" } },
+        authorize(c) {
+        if (c.password !== "password") return null
+        return {
+            id: "test",
+            name: "Test User",
+            email: "test@example.com",
+        }},
+    }),
+]
+   
+export const providerMap = providers.map((provider) => {
+    if (typeof provider === "function") {
+        const providerData = provider()
+        return { id: providerData.id, name: providerData.name }
+    } else {
+        return { id: provider.id, name: provider.name }
+    }
+})
 
-const authConfig: NextAuthConfig = {
-    providers: [
-        github
-    ],
-}
-
-export const { handlers, auth } = NextAuth(authConfig);
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    providers,
+    pages: {
+        signIn: "/signin",
+    },
+})
