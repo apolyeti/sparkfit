f277248ea1999893e5bf030d81f9d555a908f88d
apolyeti <azhandarveen@gmail.com>
2024-07-16 17:11:25 -0700

    feat: redirecting is not working whatever

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,19 +2,20 @@ import NextAuth, { NextAuthConfig} from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Credentials from "next-auth/providers/credentials";
 import type { Provider } from "next-auth/providers";
+import { OAuth2Config } from "next-auth/providers";
 
 const providers: Provider[] = [
     GitHub,
-    Credentials({
-        credentials: { password: { label: "Password", type: "password" } },
-        authorize(c) {
-        if (c.password !== "password") return null
-        return {
-            id: "test",
-            name: "Test User",
-            email: "test@example.com",
-        }},
-    }),
+    // Credentials({
+    //     credentials: { password: { label: "Password", type: "password" } },
+    //     authorize(c) {
+    //     if (c.password !== "password") return null
+    //     return {
+    //         id: "test",
+    //         name: "Test User",
+    //         email: "test@example.com",
+    //     }},
+    // }),
 ]
    
 export const providerMap = providers.map((provider) => {
@@ -26,9 +27,20 @@ export const providerMap = providers.map((provider) => {
     }
 })
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+const authConfig: NextAuthConfig = {
     providers,
     pages: {
         signIn: "/signin",
     },
-})
+    callbacks: {
+        async signIn({user, account, profile}) {
+            return true
+        },
+
+        async redirect({url, baseUrl}) {
+            return url.startsWith(baseUrl) ? url : baseUrl
+        }
+    },
+}
+
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
