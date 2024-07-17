6835d40846546041025685389ab94aa940e85e29
apolyeti <azhandarveen@gmail.com>
2024-07-16 18:21:07 -0700

    docs: clean up needless comments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,16 +6,6 @@ import { OAuth2Config } from "next-auth/providers";
 
 const providers: Provider[] = [
     GitHub,
-    // Credentials({
-    //     credentials: { password: { label: "Password", type: "password" } },
-    //     authorize(c) {
-    //     if (c.password !== "password") return null
-    //     return {
-    //         id: "test",
-    //         name: "Test User",
-    //         email: "test@example.com",
-    //     }},
-    // }),
 ]
    
 export const providerMap = providers.map((provider) => {
