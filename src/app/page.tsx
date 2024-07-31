d3dba013d0045f691472ad67cee30bf008c11dd9
Arveen Azhand <azhandarveen@gmail.com>
2024-07-30 23:52:52 -0700

    fix: remove line importing unused and deleted component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,7 @@
-"use client";
-import Link from "next/link";
+
 import GetStartedButton from "@/components/HomePageComponents/GetStartedButton";
-import SignInGithub from "@/components/SignInGithub";
-import { useSession } from "next-auth/react";
 
-export default function Home() {
-
-    const {data : session} = useSession();
-
-
-    // const [location, setLocation] = useState<UserLocationInfo | null>(null);
-    // const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
-
-    // useEffect(() => {
-    //     async function fetchWeather() {
-    //         try {
-    //             if (!API_KEY) {
-    //                 throw SparkFitError.MissingAPIKey();
-    //             }
-    //             navigator.geolocation.getCurrentPosition(async (position) => {
-    //                 const lat = position.coords.latitude;
-    //                 const lon = position.coords.longitude;
-                    
-    //                 // get weather data
-    //                 const data = await getWeatherData(API_KEY, lat, lon);
-    //                 if (data) {
-    //                     setLocation({
-    //                         city: data.location.name,
-    //                         country: data.location.country,
-    //                         weather: data.current.condition.text,
-    //                         temperature: data.current.temp_c,
-    //                         wind_speed: data.current.wind_kph,
-    //                         humidity: data.current.humidity,
-    //                     });
-    //                 }
-    //             });
-    //         }
-    //         catch (error) {
-    //             console.error(error);
-    //         }
-    //     }
-    //     fetchWeather();
-    // }, [API_KEY]);
+export default async function Home() {
 
     return (
         <div className="flex items-center justify-center h-screen flex-col">
